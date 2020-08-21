76336b27b2ac230fd017fd96731c067a75c3b7f4
westonplatter <westonplatter@gmail.com>
2020-08-21 16:59:35 -0600

    change host name

--- test_me.py
+++ test_me.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
 def test_x():
-    cnx_str = "postgres://postgres_user:postgres_password@localhost:5432/db_test"
+    cnx_str = "postgres://postgres_user:postgres_password@postgres:5432/db_test"
     engine = create_engine(cnx_str)
 
     with engine.connect() as con:
